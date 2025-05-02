77323a3d49c1d6bb0691f292f7c42a43676c9ff6
noravitkai <norvit01@easv365.dk>
2025-05-02 11:24:07 +0200

    Configure CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
 import express, { Application } from "express";
 import dotenvFlow from "dotenv-flow";
+import cors from "cors";
 import routes from "./routes";
 import { connect } from "./repository/database";
 
 dotenvFlow.config();
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "auth-token",
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+    ],
+  })
+);
+app.options("*", cors());
 
 app.use(express.json());
 app.use("/api", routes);
